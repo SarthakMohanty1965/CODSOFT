0a7f43d3b63d93957e85e60883be414ff11cb242
Sarthak Mohanty <mohantysarthak781@gmail.com>
2024-07-06 22:22:21 +0530

    Updated pubspec,yaml for Images
    testing Image filter functioning in this phase

--- task_04/lib/main.dart
+++ task_04/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -33,89 +36,99 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        iconTheme: IconThemeData(color: Colors.indigo),
-        title: Container(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(width: 5, color: Colors.black12),
-                borderRadius: BorderRadius.circular(3),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 4,
-                      spreadRadius: 1,
-                      offset: Offset(2, 4),
-                      color: Colors.white)
-                ]),
-            child: Text(
-              ' Chat app ',
-              style: GoogleFonts.acme(),
-            )),
-      ),
-      drawer: Drawer(
-        shadowColor: Colors.black,
-        elevation: 12,
-        backgroundColor: Colors.white54,
-      surfaceTintColor: Colors.indigoAccent.shade700,
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.bottomRight,
-                width: double.infinity ,height:100,
-                child: Image.asset('image/drawer_image.jpg',fit:BoxFit.fill,),
-
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Expanded(
-                  child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 18,
-                      left: 18.0,
-                      right: 18,
-                    ),
-                    child: Card(
-
-                      child: Text(
-                        ' Settings ',
-                        style: GoogleFonts.abel(textStyle: TextStyle(fontSize: 18)),
-                      ),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-                      elevation: 12,
-
-
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 18, left: 18.0, right: 18),
-                    child: Card(
-                      child: Text(
-                        ' About Me ',
-                        style: GoogleFonts.abel(textStyle: TextStyle(fontSize: 18)),
-                      ),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-                      elevation: 12,
-                    ),
-                  )
-                ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          iconTheme: IconThemeData(color: Colors.indigo),
+          title: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(width: 5, color: Colors.black12),
+                  borderRadius: BorderRadius.circular(3),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 4,
+                        spreadRadius: 1,
+                        offset: Offset(2, 4),
+                        color: Colors.white)
+                  ]),
+              child: Text(
+                ' Chat app ',
+                style: GoogleFonts.acme(),
               )),
+        ),
+        drawer: Drawer(
+          shadowColor: Colors.black,
+          elevation: 0,
+          backgroundColor: Colors.black54,
+          child: Center(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      child: Image.asset('lib/assets/image/drawer_image.jpg',
+                          fit: BoxFit.cover),
+
+                      ),
+
+
+
+                  ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 18,
+                        left: 18.0,
+                        right: 18,
+                      ),
+                      child: Card(
+                        child: Text(
+                          ' Settings ',
+                          style: GoogleFonts.abel(
+                              textStyle: TextStyle(fontSize: 18)),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4)),
+                        elevation: 12,
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 18, left: 18.0, right: 18),
+                      child: Card(
+                        child: Text(
+                          ' About Me ',
+                          style: GoogleFonts.abel(
+                              textStyle: TextStyle(fontSize: 18)),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4)),
+                        elevation: 12,
+                      ),
+                    )
+                  ],
+                )),
+              ],
+            ),
+          ),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('h'),
+              Text('h'),
             ],
           ),
-        ),),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('h'),
-            Text('h'),
-          ],
         ),
       ),
     );
