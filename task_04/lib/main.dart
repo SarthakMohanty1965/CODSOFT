754d9a05b3296b8e36197e1cc696b3f95ef480b2
Sarthak Mohanty <mohantysarthak781@gmail.com>
2024-07-06 13:01:52 +0530

    drawer, Appbar UI developed

--- task_04/lib/main.dart
+++ task_04/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,14 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -23,30 +23,103 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, });
-@override
+  const MyHomePage({
+    super.key,
+  });
+  @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('CHATAPP'),
+        backgroundColor: Colors.black,
+        title: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(width: 5, color: Colors.white70),
+                borderRadius: BorderRadius.circular(3),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 4,
+                      spreadRadius: 1,
+                      offset: Offset(2, 4),
+                      color: Colors.white)
+                ]),
+            child: Text(
+              ' Chat app ',
+              style: GoogleFonts.acme(),
+            )),
       ),
-      body:const  Center(
-         child: Column(
-         mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-             Text('h'),
-            Text('h'
+      drawer: Drawer(
+        shadowColor: Colors.black,
+        elevation: 12,
+        backgroundColor: Colors.white70,
+      surfaceTintColor: Colors.teal.shade600,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child:SizedBox(height: 12,),
+                  color: Colors.red,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 18,
+                        left: 18.0,
+                        right: 18,
+                      ),
+                      child: Card(
+
+                        child: Text(
+                          ' Settings ',
+                          style: GoogleFonts.abel(textStyle: TextStyle(fontSize: 18)),
+                        ),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                        elevation: 12,
+
+
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 18, left: 18.0, right: 18),
+                      child: Card(
+                        child: Text(
+                          ' About Me ',
+                          style: GoogleFonts.abel(textStyle: TextStyle(fontSize: 18)),
+                        ),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                        elevation: 12,
+                      ),
+                    )
+                  ],
+                ))
+              ],
             ),
+          ),
+        ),),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('h'),
+            Text('h'),
           ],
         ),
       ),
-       );
+    );
   }
 }
